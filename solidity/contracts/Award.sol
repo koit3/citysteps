4520aad84d04c5b27f7d6fb2f42fa7a4822de6e5
Koit <0xkoit3@gmail.com>
2022-09-25 08:38:22 +0900

    Award contract skelton

---
+++ solidity/contracts/Award.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import "hardhat/console.sol";
+
+contract Award {
+
+    function lottery() public {
+      // check the sender has necessary NFTs
+      // give award NFT to sender if they win the award
+    }
+}
